f27d50a4db8821630007711407d44531e071f4cb
ksidharth8 <btech10180.23@bitmesra.ac.in>
2025-02-04 12:20:24 +0530

    add comments in next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,9 @@
+// Purpose: Define the shape of the session object that is returned by the NextAuth.js library.
+
+// Importing the required modules.
 import { DefaultSession } from "next-auth";
 
+// Extending the session object.
 declare module "next-auth" {
 	interface Session {
 		user: {
@@ -7,4 +11,6 @@ declare module "next-auth" {
 		} & DefaultSession["user"];
 	}
 }
+
+// Exporting the module.
 export {};
